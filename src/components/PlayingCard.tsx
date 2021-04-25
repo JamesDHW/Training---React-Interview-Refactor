5609170c0d6b425136e48e1098595c4ba4db7d40
JamesDHW <JamesHaworthWheatman@gmail.com>
2021-04-25 20:54:34 +0100

    ternary > func

--- src/components/PlayingCard.tsx
+++ src/components/PlayingCard.tsx
@@ -6,14 +6,13 @@ interface Props {
 }
 
 const PlayingCard: FC<Props> = ({ value }) => {
-  const getCardDigit = (value: number): string => {
-    return value < 11
+  const cardString =
+    value < 11
       ? value.toString()
       : FACE_CARDS[value as keyof typeof FACE_CARDS];
-  };
   return (
     <div className="card m-1">
-      <div className="card-body">{getCardDigit(value)}</div>
+      <div className="card-body">{cardString}</div>
     </div>
   );
 };
